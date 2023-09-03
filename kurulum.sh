3d4aaf6bc6425dd6ab80e62f07e719bb2c464ee4
Cyber <55909183+scriptkidsensei@users.noreply.github.com>
2023-09-03 17:22:57 +0300

    Update kurulum.sh

--- kurulum.sh
+++ kurulum.sh
@@ -1,4 +1,5 @@
 #Bu scripti "sudo ./kurulum.sh" ile çalıştırabilirsiniz
+echo Kurulum başladı
 sudo pacman -S python3 
 
 sudo pacman -S python-colorama --noconfirm
@@ -12,4 +13,4 @@ chmod 777 *
 cd ..
 
 clear
-echo Kurulum tamamlandı. "sudo python3 EndArch.py" komudu ile scripti çalıştırabilirsiniz. 
+echo Kurulum tamamlandı. "sudo python3 EndArch.py" komudu ile scripti çalıştırabilirsiniz.
