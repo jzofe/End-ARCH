977da15f0f41dba96cc23ac7f47a9d9e0274e3db
Cyber <55909183+scriptkidsensei@users.noreply.github.com>
2023-09-03 17:20:48 +0300

    Update kurulum.sh

--- kurulum.sh
+++ kurulum.sh
@@ -1,8 +1,15 @@
 #Bu scripti "sudo ./kurulum.sh" ile çalıştırabilirsiniz
+sudo pacman -S python3 
+
 sudo pacman -S python-colorama --noconfirm
+
 chmod +x EndArch.py
+
 cd /bin/ 
-chmod +x EnderTrafficViewer.py
+
+chmod 777 *
+
 cd ..
+
 clear
-echo Kurulum tamamlandı. python3 EndArch.py komudu ile scripti çalıştırabilirsiniz. 
+echo Kurulum tamamlandı. "sudo python3 EndArch.py" komudu ile scripti çalıştırabilirsiniz. 
